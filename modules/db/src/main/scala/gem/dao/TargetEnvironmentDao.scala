d3840ce3a22a719f6bc9fa0405b3e338b34dec63
Shane Walker <swalker@gemini.edu>
2018-01-11 15:40:47 -0300

    TargetEnvironmentDao

---
+++ modules/db/src/main/scala/gem/dao/TargetEnvironmentDao.scala
@@ -0,0 +1,33 @@
+// Copyright (c) 2016-2017 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package gem
+package dao
+
+import gem.enum.Site
+import gem.util.InstantMicros
+
+import doobie._
+
+
+object TargetEnvironmentDao {
+
+  private def toTargetEnvironment(
+    userTargets: ConnectionIO[List[(Int, UserTarget)]]
+  ): ConnectionIO[TargetEnvironment] =
+    userTargets.map { lst =>
+      TargetEnvironment(lst.unzip._2.toSet)
+    }
+
+  def selectEmpty(oid: Observation.Id): ConnectionIO[TargetEnvironment] =
+    toTargetEnvironment(UserTargetDao.selectAllEmpty(oid))
+
+  def select(
+    oid: Observation.Id,
+    site: Site,
+    from: InstantMicros,
+    to: InstantMicros
+  ): ConnectionIO[TargetEnvironment] =
+    toTargetEnvironment(UserTargetDao.selectAll(oid, site, from, to))
+
+}
